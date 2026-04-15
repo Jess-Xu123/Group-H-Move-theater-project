82403cb29ae36de0a92ef38d848dd84889555fbe
YAKI <sliver5187@outlook.com>
2026-04-15 22:05:36 +0300

    Add files via upload

--- frontend/js/app.js
+++ frontend/js/app.js
@@ -1,101 +1,9 @@
 const API_URL = 'http://localhost:3000/api';
 
-// Mock data
-if (!localStorage.getItem('cart')) {
-    const mockCart = [
-        { id: 1, name: "Interstellar - Movie Ticket", price: 15.00, type: "Digital Ticket", qty: 1 },
-        { id: 2, name: "Combo XL (Popcorn + Soda)", price: 12.50, type: "Snacks & Drinks", qty: 2 },
-        { id: 3, name: "Serial Ticket (5 Movies)", price: 60.00, type: "Special Offer", qty: 1 }
-    ];
-    localStorage.setItem('cart', JSON.stringify(mockCart));
-}
-
-// Shopping cart function
-function renderCart() {
-    const container = document.getElementById('cartItemsContainer');
-    const emptyMsg = document.getElementById('emptyCartMessage');
-    const cart = JSON.parse(localStorage.getItem('cart')) || [];
-
-    if (cart.length === 0) {
-        container.innerHTML = '';
-        emptyMsg.classList.remove('d-none');
-        updateSummary(0);
-        return;
-    }
-
-    emptyMsg.classList.add('d-none');
-    
-    container.innerHTML = cart.map((item, index) => `
-        <div class="cart-item-card d-flex align-items-center mb-3">
-            <div class="item-info flex-grow-1">
-                <h5 class="text-gold fw-bold">${item.name}</h5>
-                <span class="badge bg-secondary mt-1">${item.type}</span>
-            </div>
-            <div class="item-price-qty d-flex align-items-center gap-4">
-                <span class="price">${(item.price * item.qty).toFixed(2)} €</span>
-                <div class="qty-control d-flex align-items-center gap-2">
-                    <i class="bi bi-dash-circle" onclick="updateQty(${index}, -1)"></i>
-                    <span class="qty-num">${item.qty}</span>
-                    <i class="bi bi-plus-circle" onclick="updateQty(${index}, 1)"></i>
-                </div>
-                <i class="bi bi-trash3 text-danger" onclick="removeItem(${index})"></i>
-            </div>
-        </div>
-    `).join('');
-
-    calculateTotal(cart);
-}
-
-// Count changes
-window.updateQty = function(index, change) {
-    let cart = JSON.parse(localStorage.getItem('cart'));
-    cart[index].qty += change;
-    
-    if (cart[index].qty < 1) cart[index].qty = 1; 
-    
-    localStorage.setItem('cart', JSON.stringify(cart));
-    renderCart();
-};
-
-// Delete items
-window.removeItem = function(index) {
-    let cart = JSON.parse(localStorage.getItem('cart'));
-    cart.splice(index, 1);
-    localStorage.setItem('cart', JSON.stringify(cart));
-    renderCart();
-};
-
-// Cart summary
-function calculateTotal(cart) {
-    const subtotal = cart.reduce((sum, item) => sum + (item.price * item.qty), 0);
-    const vat = subtotal * 0.1; 
-    const total = subtotal + vat;
-
-    document.getElementById('subtotal').innerText = `${subtotal.toFixed(2)} €`;
-    document.getElementById('vat').innerText = `${vat.toFixed(2)} €`;
-    document.getElementById('totalPrice').innerText = `${total.toFixed(2)} €`;
-}
-
-document.addEventListener('DOMContentLoaded', renderCart);
-
-// CART checkout
-window.simulateCheckout = function() {
-    const cart = JSON.parse(localStorage.getItem('cart')) || [];
-    if (cart.length === 0) {
-        alert("Your cart is empty!");
-        return;
-    }
-    
-    alert("Payment Successful! Your tickets have been sent to your email.");
-    localStorage.removeItem('cart'); 
-    renderCart();
-};
-
-
 // fetch online store items data
 async function loadStoreItems() {
     try {
-        const response = await fetch(`${API_URL}/store-items`);
+        const response = await fetch(`${API_URL}/store/store-items`);
         if (! response.ok) throw new Error ('Network connection down');
 
         const items = await response.json();
@@ -107,7 +15,7 @@ async function loadStoreItems() {
 // fetch event data 
 async function loadEvents() {
     try {
-        const response = await fetch(`${API_URL}/event-schedule`);
+        const response = await fetch(`${API_URL}/event/event-schedule`);
         const events = await response.json();
         renderCalendar(events);
     } catch(error) {
@@ -180,7 +88,7 @@ window.confirmBooking = async () => {
     if (!email) return alert("Please enter email");
 
     try {
-        const response = await fetch(`${API_URL}/book-event`, {
+        const response = await fetch(`${API_URL}/event/book-event`, {
             method: 'POST',
             headers: { 'Content-Type': 'application/json' },
             body: JSON.stringify({ scheduleId, email })
@@ -230,3 +138,86 @@ document.addEventListener('DOMContentLoaded', () => {
     });
 });
 
+function updateSummary(data) {
+    let subtotal = 0;
+
+    data.forEach(item => {
+        subtotal += Number(item.price) * Number(item.quantity);
+    });
+
+    let vat = subtotal * 0.1;
+    let total = subtotal + vat;
+
+    document.getElementById("subtotal").innerText = subtotal.toFixed(2) + " €";
+    document.getElementById("vat").innerText = vat.toFixed(2) + " €";
+    document.getElementById("totalPrice").innerText = total.toFixed(2) + " €";
+}
+
+function loadCart() {
+    fetch("/api/cart")
+        .then(res => res.json())
+        .then(data => {
+            console.log("CART:", data);
+
+            const container = document.getElementById("cartItemsContainer");
+
+            if (data.length === 0) {
+                updateSummary([]);
+                document.getElementById("emptyCartMessage").classList.remove("d-none");
+                return;
+            }
+
+            if (!container) {
+                console.error("container not found!");
+                return;
+            }
+
+            container.innerHTML = "";
+
+            data.forEach(item => {
+                container.innerHTML += `
+                <div class="cart-item-card d-flex align-items-center mb-3" data-id="${item.id}">
+
+                    <div class="item-info flex-grow-1">
+                        <h5 class="text-gold fw-bold">${item.name}</h5>
+                        <span class="badge bg-secondary mt-1">Food</span>
+                    </div>
+
+                    <div class="item-price-qty d-flex align-items-center gap-4">
+
+                        <span class="price">${item.price} €</span>
+
+                        <div class="qty-control d-flex align-items-center gap-2">
+                            <i class="bi bi-dash-circle"></i>
+                            <span class="qty-num">${item.quantity}</span>
+                            <i class="bi bi-plus-circle"></i>
+                        </div>
+
+                        <i class="bi bi-trash3 text-danger"></i>
+                    </div>
+
+                </div>
+                `;
+            });
+
+            updateSummary(data);
+        });
+}
+
+document.addEventListener("DOMContentLoaded", loadCart);
+
+document.getElementById("cartItemsContainer")
+.addEventListener("click", function (e) {
+
+    const icon = e.target.closest(".bi-trash3");
+    if (!icon) return;
+
+    const card = icon.closest(".cart-item-card");
+    const id = card.dataset.id;
+
+    fetch(`/api/cart/${id}`, {
+        method: "DELETE"
+    })
+    .then(res => res.json())
+    .then(() => loadCart());
+});
---
+++ frontend/js/frontend.js
@@ -0,0 +1,287 @@
+const API_URL_Z = "http://localhost:3000/api";
+
+window.onload = async () => {
+
+const bodyId = document.body.id;
+
+if(bodyId === "register-page"){
+    // send email code（just a test for now
+    document.getElementById("send-code-btn").addEventListener("click", () => {
+        const email = document.getElementById("reg-email").value.trim();
+        if (!email || !email.includes("@")) {
+            alert("please input a real email");
+            return;
+        };
+        
+        // for backend to send to email
+        alert("code has send to " + email);
+
+        // random code in frontend（test
+        window.generatedCode = Math.floor(100000 + Math.random() * 900000).toString();
+        console.log("code (for test):", window.generatedCode);
+    });
+
+    // create account
+    document.getElementById("create-account-btn").addEventListener("click", async () => {
+    const username = document.getElementById("reg-username").value.trim();
+    const email = document.getElementById("reg-email").value.trim();
+    const password = document.getElementById("reg-password").value;
+    const confirmPassword = document.getElementById("reg-password-confirm").value;
+    const code = document.getElementById("reg-verification-code").value.trim();
+
+    if (!username || !email || !password || !confirmPassword || !code) {
+        alert("please fill all the info");
+        return;
+    }
+
+    if (password !== confirmPassword) {
+        alert("passwords are different");
+        return;
+    }
+
+    if (code !== window.generatedCode) {
+        alert("wrong code");
+        return;
+    }
+
+    try {
+        const res = await fetch(`${API_URL_Z}/users/register`, {
+            method: "POST",
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify({ username, email, password })
+        });
+
+        const data = await res.json();
+
+        if (data.code === 0) {
+            alert("successful register, please login");
+            window.location.href = "/Login.html";
+        } else {
+            alert("register fail: " + data.message);
+        }
+    } catch (err) {
+        console.error(err);
+        alert("request fail, check backend");
+    }
+    });
+}
+
+else if(bodyId === "login-page"){
+    const loginButton = document.getElementById("login-btn");
+    //login
+    loginButton.onclick = async () => {
+    const username = document.getElementById("username").value.trim();
+    const password = document.getElementById("password").value.trim();
+
+    // check
+    if (!username || !password) {
+        alert("please input the username and password");
+        return;
+    }
+
+    try {
+        const res = await fetch(`${API_URL_Z}/users/login`, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify({ username, password })
+        });
+
+        const data = await res.json();
+
+        if (data.code === 0) {
+            alert("successful login");
+
+            localStorage.setItem("token", data.token);
+
+            localStorage.setItem("username", data.username);
+            localStorage.setItem("email", data.email);
+
+            const accountTitle = document.getElementById("account-username");
+            if (accountTitle) accountTitle.textContent = data.username;
+
+            window.location.href = "/accountZ.html";
+        } else {
+            alert(data.message);
+        }
+
+    } catch (err) {
+        console.error(err);
+        alert("request fail, check backend");
+    }
+    }
+}
+
+else if(bodyId === "account-page"){
+    const token = localStorage.getItem("token");
+
+    const profileForm = document.getElementById("profile-form");
+    const usernameInput = document.getElementById("username");
+    const emailInput = document.getElementById("email");
+    const accountTitle = document.getElementById("account-username");
+
+    const storedUsername = localStorage.getItem("username");
+    const storedEmail = localStorage.getItem("email");
+
+    if (accountTitle) accountTitle.textContent = storedUsername;
+
+    // insert default info
+    usernameInput.value = storedUsername || "Guest";
+    emailInput.value = storedEmail || "guest@example.com";
+
+    if (!token) {
+        console.log("not login, use default info");
+        document.querySelector("#profile-form button").disabled = true;
+        return;
+    }
+
+    // if has token
+    try {
+        const res = await fetch(`${API_URL_Z}/users/userinfo`, {
+            headers: {
+                "Authorization": "Bearer " + token
+            }
+        });
+
+        const data = await res.json();
+
+        if (data.code === 0) {
+            usernameInput.value = data.data.username;
+            emailInput.value = data.data.email;
+
+            localStorage.setItem("username", data.data.username);
+            localStorage.setItem("email", data.data.email);
+        } else {
+            console.log("token is invaild or user does not exists, keep default");
+        }
+    } catch (err) {
+        console.error("get userinfo error", err);
+    }
+
+    profileForm.addEventListener("submit", async (e) => {
+        e.preventDefault();
+
+        const username = usernameInput.value.trim();
+        const email = emailInput.value.trim();
+
+        if (username.length < 3) {
+            alert("Username must be at least 3 characters");
+            return;
+        }
+
+        const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+        if (!emailRegex.test(email)) {
+            alert("Invalid email format");
+            return;
+        }
+
+        const token = localStorage.getItem("token");
+        if (!token) {
+            alert("You must be logged in to save changes");
+            return;
+        }
+
+        const updatedUser = {
+            username: usernameInput.value,
+            email: emailInput.value
+        };
+
+        try {
+            const res = await fetch("${API_URL_Z}/users/update", {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                    "Authorization": "Bearer " + token
+                },
+                body: JSON.stringify(updatedUser)
+            });
+
+            const data = await res.json();
+            if (data.code === 0) {
+                alert("Profile updated successfully!");
+
+                localStorage.setItem("username", usernameInput.value);
+                localStorage.setItem("email", emailInput.value);
+
+                const accountTitle = document.getElementById("account-username");
+                if (accountTitle) accountTitle.textContent = data.username;
+            } else {
+                alert("Update failed: " + data.message);
+            }
+        } catch (err) {
+            console.error(err);
+            alert("Server error");
+        }
+    });
+
+    //for mobile content
+    const mobileContent = document.getElementById("mobile-content");
+    const navLinks = document.querySelectorAll(".sidebar .nav-link");
+
+    function isMobile() {
+        return window.innerWidth < 768;
+    }
+
+    navLinks.forEach(link => {
+        link.addEventListener("click", (e) => {
+            e.preventDefault();
+            if (!isMobile()) return;
+
+            navLinks.forEach(l => l.classList.remove("active"));
+            link.classList.add("active");
+
+            const section = link.dataset.section;
+
+            let html = "";
+            switch (section) {
+                case "profile":
+                    html = document.querySelector("main .card:nth-child(1)").outerHTML;
+                    break;
+                case "tickets":
+                    html = document.querySelector("main .card:nth-child(2)").outerHTML;
+                    break;
+                case "food":
+                    html = document.querySelector("main .card:nth-child(3)").outerHTML;
+                    break;
+                case "discount":
+                    html = document.querySelector("main .card:nth-child(4)").outerHTML;
+                    break;
+                case "favorites":
+                    html = document.querySelector("main .card:nth-child(5)").outerHTML;
+                    break;
+                case "wallet":
+                    html = document.querySelector("main .card:nth-child(6)").outerHTML;
+                    break;
+            }
+
+            mobileContent.innerHTML = html;
+
+            // change input
+            const usernameInput = mobileContent.querySelector("#username");
+            const emailInput = mobileContent.querySelector("#email");
+            const accountTitle = mobileContent.querySelector("#account-username");
+
+            const username = localStorage.getItem("username") || "Guest";
+            const email = localStorage.getItem("email") || "guest@example.com";
+
+            if (usernameInput) usernameInput.value = username;
+            if (emailInput) emailInput.value = email;
+            if (accountTitle) accountTitle.textContent = username;
+
+            mobileContent.scrollIntoView({ behavior: "smooth" });
+        });
+    });
+
+    window.addEventListener("resize", () => {
+        if (!isMobile()) {
+            mobileContent.innerHTML = "";
+        }
+    });
+    
+    }
+    else if(bodyId === "cart-page"){
+
+    }
+
+};
