86252c328ed4c3072e3f1890b6e2780825c8ec55
XUZHAOHUI <xuzhaohui@XUZHAOHUIdeMacBook-Pro.local>
2026-04-19 21:27:13 +0300

    server.js update

--- backend/server.js
+++ backend/server.js
@@ -10,7 +10,7 @@ const cartRoutes = require("./routes/cartRoutes");
 const cors = require("cors");
 const path = require('path');
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 8080;
 
 const app = express();
 app.use(express.json());
